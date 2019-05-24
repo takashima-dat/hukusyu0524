a8287653681a266c1c99cd8e2931cc768e8b1036
ozeki <hhdfgg@users.noreply.github.com>
2019-05-24 13:29:38 +0900

    動く

--- hukusyu0524/Form1.cs
+++ hukusyu0524/Form1.cs
@@ -12,9 +12,51 @@ namespace hukusyu0524
 {
     public partial class Form1 : Form
     {
+        int vx = 0;
+        int vy = 0;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //up
+            vx = 0;
+            vy = -5;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //down
+            vx = 0;
+            vy = 5;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //right
+            vx = 5;
+            vy = 0;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //left
+            vx = -5;
+            vy = 0;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
         }
     }
 }
